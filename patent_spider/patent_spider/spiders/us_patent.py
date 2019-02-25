c46e8ea41ff8363549cf4c002abb46f547e700cf
jpknwls <28238972+jpknwls@users.noreply.github.com>
2019-02-24 23:18:22 -0800

    Update us_patent.py

--- patent_spider/patent_spider/spiders/us_patent.py
+++ patent_spider/patent_spider/spiders/us_patent.py
@@ -18,7 +18,7 @@ class USPatent(CrawlSpider):
     response_url = ""
     
     # all search terms for us patent search are saved in .txt file.
-    f = open("us_patent_urls.txt")
+    f = open("patent_spider/spiders/us_patent_urls.txt")
     #f = open("test.txt")
     start_urls = [url.strip() for url in f.readlines()]
     f.close()
